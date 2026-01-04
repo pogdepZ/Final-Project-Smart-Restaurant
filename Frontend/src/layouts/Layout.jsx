871fdabe80d2c60839f61a20fd7b9a6ab00235b0
LNP0806 <lamnhatbo0123@gmail.com>
2026-01-05 00:24:47 +0700

    refactor: change import Components

--- Frontend/src/layouts/Layout.jsx
+++ Frontend/src/layouts/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
-import BottomNav from "../components/BottomNav";
+import Header from "../Components/Header";
+import BottomNav from "../Components/BottomNav";
 
 const Layout = () => {
   return (
