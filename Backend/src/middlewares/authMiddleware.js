24386c6b6e84c8ef3d5aab591e8b68314b7b4643
LNP0806 <lamnhatbo0123@gmail.com>
2026-01-11 19:47:25 +0700

    refactor: debug

--- Backend/src/middlewares/authMiddleware.js
+++ Backend/src/middlewares/authMiddleware.js
@@ -1,37 +1,47 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // Middleware kiểm tra Token
 exports.protect = (req, res, next) => {
   let token;
 
   // 1. Lấy token từ header (Authorization: Bearer <token>)
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
     try {
-      token = req.headers.authorization.split(' ')[1];
+      token = req.headers.authorization.split(" ")[1];
+      console.log("hello");
 
       // 2. Giải mã token
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
 
       // 3. Lưu thông tin user vào req để dùng ở bước sau
       req.user = decoded;
-      
+
       return next(); // Cho phép đi tiếp
     } catch (error) {
       console.error(error);
-      res.status(401).json({ message: 'Token không hợp lệ, vui lòng đăng nhập lại' });
+      res
+        .status(401)
+        .json({ message: "Token không hợp lệ, vui lòng đăng nhập lại" });
     }
   }
 
   if (!token) {
-    res.status(401).json({ message: 'Bạn chưa đăng nhập, không có quyền truy cập' });
+    res
+      .status(401)
+      .json({ message: "Bạn chưa đăng nhập, không có quyền truy cập" });
   }
 };
 
 // Middleware kiểm tra quyền Admin
 exports.adminOnly = (req, res, next) => {
-  if (req.user && req.user.role === 'admin') {
+  if (req.user && req.user.role === "admin") {
     next();
   } else {
-    res.status(403).json({ message: 'Chỉ Admin mới có quyền thực hiện thao tác này' });
+    res
+      .status(403)
+      .json({ message: "Chỉ Admin mới có quyền thực hiện thao tác này" });
   }
 };
